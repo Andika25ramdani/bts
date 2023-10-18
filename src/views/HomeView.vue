2639834eb8c563b0f40030ff25c3892356f4ca45
Andika .R <andika15ramdani@gmail.com>
2023-10-18 16:41:04 +0700

    remove checklist item

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="h-full mx-auto max-w-4xl flex flex-col">
     <ul class="p-4 rounded-xl bg-gray-100 flex flex-col gap-2 h-full">
-      <li v-for="item in checklistData" :key="item.id" class="bg-white py-1 px-4 rounded-lg text-left text-gray-800 flex justify-between">
+      <li
+        v-for="item in checklistData"
+        :key="item.id"
+        class="bg-white py-1 px-4 rounded-lg text-left text-gray-800 flex justify-between"
+      >
         <span>{{ item.name }}</span>
-        <span v-if="item.checklistCompletionStatus" class="text-green-700">selesai</span>
+        <button @click="deleteChecklistItem(item)" class="text-red-700">
+          hapus
+        </button>
       </li>
     </ul>
   </div>
@@ -53,6 +59,30 @@ export default {
         })
         .finally(() => {
           console.log('FETCH COMPLETE')
+        })
+    },
+    deleteChecklistItem: function (checklist) {
+      console.log('DELETE Checklist Start')
+      axios
+        .delete(`http://94.74.86.174:8080/api/checklist/${checklist.id}`, {
+          headers: {
+            Authorization:
+              'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6W119.i2OVQdxr08dmIqwP7cWOJk5Ye4fySFUqofl-w6FKbm4EwXTStfm0u-sGhDvDVUqNG8Cc7STtUJlawVAP057Jlg',
+          },
+        })
+        .catch((e) => {
+          console.error('FAIL TO DELETE Checklists', e)
+        })
+        .then((res) => {
+          console.log('DELETE RESULT', res)
+          console.log('DELETE RESULT DATA', res.data)
+
+          this.checklistData = this.checklistData.filter(
+            (item) => item != checklist
+          )
+        })
+        .finally(() => {
+          console.log('DELETE COMPLETE')
         })
     },
   },
